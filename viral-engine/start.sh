c7546349ca582f8a3907dfccce05fe394664945e
PRAVIN RAJ <u2102721@siswa.um.edu.my>
2026-02-22 04:56:07 +0800

    typo fix

--- viral-engine/start.sh
+++ viral-engine/start.sh
@@ -3,4 +3,4 @@
 celery -A celery_worker.celery_app worker --loglevel=info --concurrency=1 --max-memory-per-child=200000 &
 
 # Start the FastAPI server
-uvicorn main.py:app --host 0.0.0.0 --port 8000
+uvicorn main:app --host 0.0.0.0 --port 8000
